46d49e19acb2cb085c3e2b2f0dd23707ab2fb3e8
KilliLogan <ivanov-p-a@bk.ru>
2022-09-16 12:17:06 +0300

    Перевел рекурсию в циклы, оптимизировал код

--- Tic Tac Toe/Program.cs
+++ Tic Tac Toe/Program.cs
@@ -48,21 +48,19 @@ namespace Tic_Tac_Toe
             {
                 if (winner)
                 {
-                    Player1();
+                    ChoosingCoordinates(winner);
                     setAxic();
                     PrintField(positionX, positionY);
                     LogicGame();
                 }
                 else
                 {
-                    Player2();
+                    ChoosingCoordinates(winner);
                     setAxic();
                     PrintField(positionX, positionY);
                     LogicGame();
                 }
             }
-
-            RestartGame();
         }
 
         private static void StartGameVsCompucter()
@@ -78,7 +76,7 @@ namespace Tic_Tac_Toe
             {
                 if (winner)
                 {
-                    Player1();
+                    ChoosingCoordinates(winner);
                     setAxic();
                     PrintField(positionX, positionY);
                     LogicGame();
@@ -96,77 +94,91 @@ namespace Tic_Tac_Toe
         private static bool SetGame()
         {
             byte setGamneTemp = 0;
+            bool success = false;
 
             Console.WriteLine("Выберете режим игры:\n1. Игрок против Игрока.\n2. Игрок против Компьютера.");
-            
-            try
+
+            while (!success)
             {
-                setGamneTemp = Convert.ToByte(Console.ReadLine());            
-            }
-            catch
-            {
-                Console.WriteLine("Введены неверные символы, повторите ввод.");
-                SetGame();
+                try
+                {
+                    setGamneTemp = Convert.ToByte(Console.ReadLine());
+
+                    switch (setGamneTemp)
+                    {
+                        case 1:
+                            return true;
+
+                        case 2:
+                            return false;
+                        default:
+                            Console.WriteLine("Введено неверное значение, повторите ввод.");
+                            break;
+
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Введены неверные символы, повторите ввод.");
+                }
             }
 
-            switch (setGamneTemp)
-            {
-                case 1:
-                    return true;
-
-                case 2:
-                    return false;
-
-                default:
-                    Console.WriteLine("Выбран неизвестный режим игры, попробуйте выбрать другой.");
-                    SetGame();
-                    return false;
-            }
+            return false;
         }
 
         private static void SetAxicX()
         {
             Console.Write("Выберите размер полеля по оси \"X\" от 3 до 9: ");
-            try
-            {
-                axicX = Convert.ToByte(Console.ReadLine());
-                axicX++;
+            bool success = false;
 
-                if (axicX <= 10 && axicX >= 4)
-                { }
-                else
-                {
-                    Console.WriteLine("Выбрано число вне диапазона, пожалуйста, введите подходящее.");
-                    SetAxicX();
-                }
-            }
-            catch
+            while(!success)
             {
-                Console.WriteLine("Введены неверные символы, повторите ввод.");
-                SetAxicX();
+                try
+                {
+                    axicX = Convert.ToByte(Console.ReadLine());
+                    axicX++;
+
+                    if (axicX <= 10 && axicX >= 4)
+                    {
+                        success = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Выбрано число вне диапазона, пожалуйста, введите подходящее.");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Введены неверные символы, повторите ввод.");
+                }
             }
         }
 
         private static void SetAxicY()
         {
             Console.Write("Выберите размер полеля по оси \"Y\" от 3 до 9: ");
-            try
-            {
-                axicY = Convert.ToByte(Console.ReadLine());
-                axicY++;
+            bool success = false;
 
-                if (axicY <= 10 && axicY >= 4)
-                { }
-                else
-                {
-                    Console.WriteLine("Выбрано число вне диапазона, пожалуйста, введите подходящее.");
-                    SetAxicY();
-                }
-            }
-            catch
+            while (!success)
             {
-                Console.WriteLine("Введены неверные символы, повторите ввод.");
-                SetAxicY();
+                try
+                {
+                    axicY = Convert.ToByte(Console.ReadLine());
+                    axicY++;
+
+                    if (axicY <= 10 && axicY >= 4)
+                    {
+                        success = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Выбрано число вне диапазона, пожалуйста, введите подходящее.");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Введены неверные символы, повторите ввод.");
+                }
             }
         }
 
@@ -226,8 +238,6 @@ namespace Tic_Tac_Toe
                 else
                 {
                     Console.WriteLine("\nНельзя выбрать это поле, выберете другое.");
-                    Player1();
-                    setAxic();
                 }
             }
             else
@@ -243,7 +253,7 @@ namespace Tic_Tac_Toe
                     if (setGame)
                     {
                         Console.WriteLine("\nНельзя выбрать это поле, выберете другое.");
-                        Player2();
+                        ChoosingCoordinates(winner);
                         setAxic();
                     }
                     else
@@ -255,122 +265,141 @@ namespace Tic_Tac_Toe
             }
         }
 
+        private static void ChoosingCoordinates(bool user)
+        {
+            bool successX = false;
+            bool successY = false;
+
+            if (user)
+            {
+                Console.Write("Игрок 1:\nВыберете координаты по оси \"X\": ");
+            }
+            else
+            {
+                Console.Write("Игрок 2:\nВыберете координаты по оси \"X\": ");
+            }
+
+            while (!successX)
+            {
+                try
+                {
+                    positionX = Convert.ToByte(Console.ReadLine());
+                    if (positionX > 0 && positionX < axicX)
+                    {
+                        successX = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Выбрано значение превышающее ширину поля, повторите ввод.");
+                    }
+
+                }
+                catch
+                {
+                    Console.WriteLine("Введены неверные символы, повторите ввод.");
+                }
+            }
+
+            Console.Write("Выберете координаты по оси \"Y\": ");
+
+            while (!successY)
+            {
+                try
+                {
+                    positionY = Convert.ToByte(Console.ReadLine());
+
+                    if (positionY > 0 && positionY < axicY)
+                    {
+                        successY = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Выбрано значение превышающее высоту поля, повторите ввод.");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Введены неверные символы, повторите ввод.");
+                }
+            }
+        }
+
         private static void Compucter()
         {
             positionX = (byte)random.Next(axicX);
             positionY = (byte)random.Next(axicY);
         }
-        private static void Player1()
-        {
-            Console.Write("Игрок 1:\nВыберете координаты по оси \"X\": ");
-            try
-            {
-                positionX = Convert.ToByte(Console.ReadLine());
-
-            }
-            catch
-            {
-                Console.WriteLine("Введены неверные символы, повторите ввод.");
-                Player1();
-            }
-
-            Console.Write("Выберете координаты по оси \"Y\": ");
-            try
-            {
-                positionY = Convert.ToByte(Console.ReadLine());
-            }
-            catch
-            {
-                Console.WriteLine("Введены неверные символы, повторите ввод.");
-                Player1();
-            }
-        }
-
-        private static void Player2()
-        {
-            Console.Write("Игрок 2:\nВыберете координаты по оси \"X\": ");
-            try
-            {
-                positionX = Convert.ToByte(Console.ReadLine());
-
-            }
-            catch
-            {
-                Console.WriteLine("Введены неверные символы, повторите ввод.");
-                Player2();
-            }
-
-            Console.Write("Выберете координаты по оси \"Y\": ");
-            try
-            {
-                positionY = Convert.ToByte(Console.ReadLine());                               
-            }
-            catch
-            {
-                Console.WriteLine("Введены неверные символы, повторите ввод.");
-                Player2();
-            }
-        }
-
+        
         private static void LogicGame()
         {
             byte count = 0;
+            bool success = false;
 
-            // Проверка по вертикали
-            for (int y = 0; y < axicY; y++)
+            while (!success)
             {
-                for (int x = 0; x < axicX - 2; x++)
-                { 
-                    if (field[y, x] != null && field[y, x].Equals(field[y, x + 1]) && field[y, x].Equals(field[y, x + 2]))
-                    {
-                        startGame = false;
-                        PrintWinner();
-                    }
-                }
-            }
-
-            // Проверка по горизонтали
-            for (int y = 0; y < axicY - 2; y++)
-            {
-                for (int x = 0; x < axicX; x++)
+                // Проверка по вертикали
+                for (int y = 0; y < axicY; y++)
                 {
-                    if (field[y, x] != null && field[y, x].Equals(field[y + 1, x]) && field[y, x].Equals(field[y + 2, x]))
+                    for (int x = 0; x < axicX - 2; x++)
                     {
-                        startGame = false;
-                        PrintWinner();
-                    }
-                }
-            }
-
-            // Проверка по диагоналям
-            for (int y = 1; y < axicY - 1; y++)
-            {
-                for (int x = 1; x < axicX - 1; x++)
-                {
-                    if ((field[y, x] != null && field[y, x].Equals(field[y + 1, x + 1]) && field[y, x].Equals(field[y - 1, x - 1])) ||
-                        (field[y, x] != null && field[y, x].Equals(field[y - 1, x + 1]) && field[y, x].Equals(field[y + 1, x - 1])))
-                    {
-                        startGame = false;
-                        PrintWinner();
-                    }
-                }
-            }
-
-            // Проверка на нечью
-            for (int y = 0; y < axicY; y++)
-            {                
-                for (int x = 0; x < axicX; x++)
-                {
-                    if (field[y, x] != null)
-                    {
-                        count++;
-                        if (count == axicX * axicY)
+                        if (field[y, x] != null && field[y, x].Equals(field[y, x + 1]) && field[y, x].Equals(field[y, x + 2]))
                         {
-                            Console.WriteLine("Ничья!");
                             startGame = false;
+                            PrintWinner();
+                            success = true;
+                            return;
                         }
                     }
                 }
+
+                // Проверка по горизонтали
+                for (int y = 0; y < axicY - 2; y++)
+                {
+                    for (int x = 0; x < axicX; x++)
+                    {
+                        if (field[y, x] != null && field[y, x].Equals(field[y + 1, x]) && field[y, x].Equals(field[y + 2, x]))
+                        {
+                            startGame = false;
+                            PrintWinner();
+                            success = true;
+                            return;
+                        }
+                    }
+                }
+
+                // Проверка по диагоналям
+                for (int y = 1; y < axicY - 1; y++)
+                {
+                    for (int x = 1; x < axicX - 1; x++)
+                    {
+                        if ((field[y, x] != null && field[y, x].Equals(field[y + 1, x + 1]) && field[y, x].Equals(field[y - 1, x - 1])) ||
+                            (field[y, x] != null && field[y, x].Equals(field[y - 1, x + 1]) && field[y, x].Equals(field[y + 1, x - 1])))
+                        {
+                            startGame = false;
+                            PrintWinner();
+                            success = true;
+                            return;
+                        }
+                    }
+                }
+
+                // Проверка на нечью
+                for (int y = 0; y < axicY; y++)
+                {
+                    for (int x = 0; x < axicX; x++)
+                    {
+                        if (field[y, x] != null)
+                        {
+                            count++;
+                            if (count == axicX * axicY)
+                            {
+                                Console.WriteLine("Ничья!");
+                                startGame = false;
+                            }
+                        }
+                    }
+                }
+                success = true;
             }
         }
 
@@ -380,7 +409,7 @@ namespace Tic_Tac_Toe
             {
                 Console.WriteLine("Победель: Игрок 1!");
             } 
-            else if (!winner && setGame)
+            else if (winner && setGame)
             {
                 Console.WriteLine("Победель: Игрок 2!");
             }
@@ -391,23 +420,29 @@ namespace Tic_Tac_Toe
 
         private static void RestartGame()
         {
-            Console.Write("\nХотите сыграть еще раз?\nНажмите: \"Y\" - если ДА, \"N\" - если НЕТ: ");
-            string userSelection = Console.ReadLine();
+            bool success = false;
 
-            if (userSelection.Equals("y") || userSelection.Equals("Y"))
+            while (!success)
             {
-                startGame = true;
-                firstInit = true;
-                Console.WriteLine();
-            } 
-            else if (userSelection.Equals("n") || userSelection.Equals("N"))
-            {
-                startGame = false;
-            }
-            else
-            {
-                Console.WriteLine("\nВведен неверный символ, повторите ввод.");
-                RestartGame();
+                Console.Write("\nХотите сыграть еще раз?\nНажмите: \"Y\" - если ДА, \"N\" - если НЕТ: ");
+                string userSelection = Console.ReadLine();
+
+                if (userSelection.Equals("y") || userSelection.Equals("Y"))
+                {
+                    startGame = true;
+                    firstInit = true;
+                    Console.WriteLine();
+                    success = true;
+                }
+                else if (userSelection.Equals("n") || userSelection.Equals("N"))
+                {
+                    startGame = false;
+                    success = true;
+                }
+                else
+                {
+                    Console.WriteLine("\nВведен неверный символ, повторите ввод.");
+                }
             }
         }
     }
